f9d4656cb397b96b503032e7f6f59df52a6c0776
Sabira1iyev <s.aliyev2005@gmail.com>
2025-10-31 07:38:55 -0400

    Student rating commit-2

--- main.cpp
+++ main.cpp
@@ -19,10 +19,33 @@ class Teacher: public Login{
     public:
     Teacher(string n, string u, string p, string tLessN):
     Login(n,u,p),TeacherlessonName(tLessN){}
+    
+    string getTeacherlessonName(){
+        return TeacherlessonName;
+    }
+
 };
 
 
 
+
 int main(){
+string username = "Sabir ALiyev";
+int password = 12345678;
+
+string usName;
+int pass;
+
+cout << "Enter username: ";
+getline(cin, usName);
+cout << "Enter password: ";
+cin >> pass;
+
+if(usName == username && pass == password){
+    cout << "Success" << endl;
+}
+else{
+    cout << "Wrong password, try again" << endl;
+}
 
 }
