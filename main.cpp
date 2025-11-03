906bda4ed104f0d0073a246df4bf836aca431b54
Sabira1iyev <s.aliyev2005@gmail.com>
2025-11-03 12:41:05 -0500

    added virtual functions

--- main.cpp
+++ main.cpp
@@ -6,24 +6,26 @@ using namespace std;
 class Login{
     public:
     string name;
-    string username;
-    string password;
-    Login(string n, string u, string pass):
-    name(n),username(u),password(pass){}
-
+     string password;
+    Login(string n, string pass):
+    name(n),password(pass){}
+    
+    virtual void Display(){
+        cout << "User: " << name << endl;
+    }
 };
 
 class Teacher: public Login{
     private:
     string TeacherlessonName;
     public:
-    Teacher(string n, string u, string p, string tLessN):
-    Login(n,u,p),TeacherlessonName(tLessN){}
+    Teacher(string n, string p, string tLessN):
+    Login(n,p),TeacherlessonName(tLessN){}
     
     string getTeacherlessonName(){
         return TeacherlessonName;
     }
-    void Display(){
+    void Display()override{
         cout << "Teacher's name: " << name << endl;
         cout << "Which Lesson: " << TeacherlessonName << endl;
     }
@@ -33,8 +35,12 @@ class Student: public Login{
     private:
         vector<pair<string, double>> grades;
         public:
-        Student(string n, string u, string p):
-            Login(n,u,p){}
+        Student(string n, string p):
+            Login(n,p){}
+        
+            void Display()override{
+                cout << "Student's name: " << name << endl;
+             }
 };
 
 
@@ -53,6 +59,8 @@ cin >> pass;
 
 if(usName == username && pass == password){
     cout << "Success" << endl;
+    Teacher T1("James", "12444", "Mathmematic");
+    T1.Display();
 }
 else{
     cout << "Wrong password, try again" << endl;
